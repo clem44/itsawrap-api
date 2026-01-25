28af8cab576bef2d6229b4b5a3d1051bbf2ec12e
Clemaurde Gumbs <clem.gumbs@gmail.com>
2026-01-25 18:04:08 -0400

    Update DB Seeders

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -10,14 +10,14 @@ class CategorySeeder extends Seeder
     public function run(): void
     {
         $categories = [
-            ['name' => 'Wraps', 'sort_order' => 1],
-            ['name' => 'Bowls', 'sort_order' => 2],
-            ['name' => 'Salads', 'sort_order' => 3],
-            ['name' => 'Sides', 'sort_order' => 4],
-            ['name' => 'Sauces', 'sort_order' => 5],
-            ['name' => 'Burgers', 'sort_order' => 6],
-            ['name' => 'Merchandise', 'sort_order' => 7],
-            ['name' => 'Other', 'sort_order' => 8],
+            ['name' => 'Wraps', 'sort_order' => 1, 'color' => "#FC8326", 'icon' => 'wraps'],
+            ['name' => 'Bowls', 'sort_order' => 2, 'color' => "#4CAF50", 'icon' => 'bowls'],
+            ['name' => 'Salads', 'sort_order' => 3, 'color' => "#4CAF50", 'icon' => 'salads'],
+            ['name' => 'Sides', 'sort_order' => 4, 'color' => "#4CAF50", 'icon' => 'side'],
+            ['name' => 'Sauces', 'sort_order' => 5, 'color' => "#FC8326", 'icon' => 'sauce'],
+            ['name' => 'Burgers', 'sort_order' => 6, 'color' => "#FC8326", 'icon' => 'burgers'],
+            ['name' => 'Merchandise', 'sort_order' => 7, 'color' => "#795548", 'icon' => "merchandise"],
+            ['name' => 'Other', 'sort_order' => 8, 'color' => "#607D8B"],
         ];
 
         foreach ($categories as $category) {
